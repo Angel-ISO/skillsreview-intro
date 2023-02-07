ee48e67c67c4e0ccf30c6464069fe1d69596a3d5
angelgabrielortegacorzoCAMPUS <angelgabrielorteg@gmail>
2023-02-07 07:20:38 -0500

    actividad

---
+++ ejercisios.py
@@ -0,0 +1,13 @@
+# 1. Qué operadores utiliza Python en los siguientes casos:
+# A. División Modular
+# rta: se usa la barra de porcentaje %
+
+# B. Exponenciación
+# rta: se usa el doble asterizco **
+
+# C. División que retorne entero.
+# rta: se usa el doble slahs //
+
+for divisionModular in range(1, 10):
+    if(divisionModular% 2 != 0):
+        print(divisionModular)
